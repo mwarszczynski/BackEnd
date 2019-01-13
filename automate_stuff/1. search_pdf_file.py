5f17d114b1cd162382632fa3e9f76037402846f2
mwarszczynski <m.warszczynski@gmail.com>
2019-01-13 09:59:35 +0100

    Final version

--- automate_stuff/1. search_pdf_file.py
+++ automate_stuff/1. search_pdf_file.py
@@ -1,18 +1,23 @@
-# Script that searches all .pdf files on mentioned disk [Currently works only in Windows].
+# Script that searches all .pdf files on mentioned disk [For now this scripts will work only on win os].
 import os
 
-def dirThrough(text):
+def dirThrough(text, counter=0):
     for root, dirs, files in os.walk(text):
         for file in files:
-            if file.endswith('pdf'):
-                print(os.path.join(root, file))
+            if file.endswith(fileType):
+                counter += 1
+                print('['+str(counter)+']. ', os.path.join(root, file))
 
-c = input('\nType the path of Your disc which You want to research: \t\t(write like " D:\\ ". )\n')
+fileType = input('\nType the file that You want to find: \n')
+c = input('\nType the path of Your disc which You want to research: \t\t(example " D:\\ ". )\n')
+d = 'Total: '
 
 os.chdir(c)
 currentPath = os.getcwd()
 print('\nLets search pdf file in the current path, which is: %s\n' % currentPath)
 print(dirThrough(currentPath))
+
+input('Press enter to close program')
 
 
 
