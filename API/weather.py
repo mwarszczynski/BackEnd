ea904283886ab8bcf89212047c8fe53412276722
mwarszczynski <m.warszczynski@gmail.com>
2019-01-21 16:49:27 +0100

    Upgrade

--- API/weather.py
+++ API/weather.py
@@ -1,21 +1,22 @@
-Iimport requests
+import requests
 from pprint import pprint
 
 city = input('Enter your City : ')
 
-# Here You have to type suitable api key, that cen be generated on apiweather web page
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=<ENTER_OWN_API_KEY>'.format(city)
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=7ea290f5bb54f4d7562ae933a868a038'.format(city)
 
 res = requests.get(url)
 data = res.json()
 
 currentTemp = data['main']['temp']
+pressure = data['main']['pressure']
 windSpeed = data['wind']['speed']
 
 latitude = data['coord']['lat']
 longitude = data['coord']['lon']
 
-print('Temperature : {} degree celcius'.format(currentTemp))
+print('Temperature :', currentTemp)
 print('Wind speed : {} m/s'.format(windSpeed))
+print('Pressure : {} hPa'.format(pressure))
 print('Latitude : {}'.format(latitude))
 print('Longitude : {}'.format(longitude))
