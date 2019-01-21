566e92672931fde304b0db9c325eb5cadd785131
mwarszczynski <m.warszczynski@gmail.com>
2019-01-21 19:15:18 +0100

    Division into functions, add geoLocation and so on

--- API/weather.py
+++ API/weather.py
@@ -1,22 +1,51 @@
 import requests
-from pprint import pprint
 
-city = input('Enter your City : ')
+# Function search the City that You enter, and return info from openweather API about current weather
+def findOverCity():
+    yourCity = input('Pass Your City : ')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=7ea290f5bb54f4d7562ae933a868a038'.format(yourCity)
+    res = requests.get(url)
+    data = res.json()
+    showInfo(data)
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=7ea290f5bb54f4d7562ae933a868a038'.format(city)
+# Function uses 'ipinfo' webpage to check the geolocation, and return info from openweather API about current weather
+def findOverCurrentLocation():
+    url_location = ('https://ipinfo.io/')
+    res = requests.get(url_location)
+    data = res.json()
 
-res = requests.get(url)
-data = res.json()
+    place = data['loc'].split(',')
+    latitude = place[0]
+    longitude = place[1]
 
-currentTemp = data['main']['temp']
-pressure = data['main']['pressure']
-windSpeed = data['wind']['speed']
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=7ea290f5bb54f4d7562ae933a868a038&units=metric'.format(latitude, longitude)
+    res = requests.get(url)
+    data = res.json()
 
-latitude = data['coord']['lat']
-longitude = data['coord']['lon']
+    showInfo(data)
 
-print('Temperature :', currentTemp)
-print('Wind speed : {} m/s'.format(windSpeed))
-print('Pressure : {} hPa'.format(pressure))
-print('Latitude : {}'.format(latitude))
-print('Longitude : {}'.format(longitude))
+# This is summary function, which is concludes all information downloaded from openweather API
+def showInfo(data):
+    currentTemperature = data['main']['temp']
+    windSpeed = data['wind']['speed']
+    currentPressure = data['main']['pressure']
+
+    print('Current temperature : ', currentTemperature)
+    print('Wind speed: {} m/s'.format(windSpeed))
+    print('Current pressure: ', currentPressure)
+
+def main():
+    print('1. Get data by city')
+    print('2. Get data by location')
+
+    choice = input('Type Your choice : ')
+
+    if choice == '1':
+        findOverCity()
+    elif choice == '2':
+        findOverCurrentLocation()
+    else:
+        print('Invalid...')
+
+if __name__ == '__main__':
+    main()
