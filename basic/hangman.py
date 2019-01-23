1eb4f54dd56de82814a561f36c4373591a2ba91d
m.warszczynski <m.warszczynski@gmail.com>
2019-01-23 14:48:51 +0100

    simply hangman in cmd

---
+++ basic/hangman.py
@@ -0,0 +1,3 @@
+# 1. First player invents word, and write - how many char are contained
+# 2. If second player will guess the letter, first player appends this char to 'guessed'
+# 3. If quantity of guesses is equal to the number of words given to the player - break ('hangman done')
