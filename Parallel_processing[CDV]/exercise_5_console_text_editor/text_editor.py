9b534c236826077eb8590a5a7c0c38dff4688c61
mwarszczynski <m.warszczynski@gmail.com>
2019-03-24 15:05:30 +0100

    extending feature

--- Parallel_processing[CDV]/exercise_5_console_text_editor/text_editor.py
+++ Parallel_processing[CDV]/exercise_5_console_text_editor/text_editor.py
@@ -1,9 +1,26 @@
-from tkinter import *
-import tkinter.scrolledtext as ScrolledText
+from tkinter import Tk, scrolledtext, Menu, filedialog, END
 
 # creating object, main window
 root = Tk()
-textArea = ScrolledText.ScrolledText(root, width=100, height=50)
+textArea = scrolledtext.ScrolledText(root, width=100, height=50)
+
+# Functions
+def openFile():
+    file = filedialog.askopenfile(parent=root, mode='rb', title='Select a text file')
+
+    if file != None:
+        contents = file.read()
+        textArea.insert('1.0', contents)
+        file.close()
+
+def saveFile():
+    file = filedialog.asksaveasfile(mode='w')
+
+    if file != None:
+        # slice off the last character from get, as an extra return (enter) is added.
+        data = textArea.get('1.0', END+'-1c')
+        file.write(data)
+        file.close()
 
 # Menu options
 menu = Menu(root)
@@ -12,8 +29,8 @@ fileMenu = Menu(menu)
 menu.add_cascade(label="File", menu=fileMenu)
 
 fileMenu.add_command(label="New")
-fileMenu.add_command(label="Open")
-fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Open", command=openFile)
+fileMenu.add_command(label="Save",  command=saveFile)
 fileMenu.add_command(label="Save as...")
 fileMenu.add_separator()
 fileMenu.add_command(label="Page settings")
@@ -21,6 +38,9 @@ fileMenu.add_command(label="Print")
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit")
 
+helpMenu = Menu(menu)
+menu.add_cascade(label="Help")
+menu.add_cascade(label="About")
 
 textArea.pack()
 
