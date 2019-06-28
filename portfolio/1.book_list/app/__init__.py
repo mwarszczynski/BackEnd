52cce1f33175a62b68e98091c72256898a7b566f
mwarszczynski <m.warszczynski@gmail.com>
2019-06-28 02:06:02 -0700

    refresh changes version 1.2

--- portfolio/1.book_list/app/__init__.py
+++ portfolio/1.book_list/app/__init__.py
@@ -2,14 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-# from flask_bcrypt import Bcrypt
-# from flask_bcrypt import *
-# from flask_login import LoginManager
-
 
 db = SQLAlchemy()
-# bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
 
 
 def create_app(config_class=Config):
@@ -17,9 +11,8 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
-    # with app.app_context():
-    # Extensions like Flask-SQLAlchemy now know what the "current" app
-    # is while within this block. Therefore, you can now run........
+    with app.app_context():
+        init_db()
         # db.create_all()
 
     from app.users.routes import users
--- portfolio/1.book_list/app/models.py
+++ portfolio/1.book_list/app/models.py
@@ -1,13 +1,6 @@
 from datetime import datetime
 from flask import current_app
 from app import db 
-# #from app import login_manager
-# from flask_login import UserMixin
-
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#         return User.query.get(int(user_id))
 
 
 books_authors = db.Table('BooksAuthors',
@@ -52,16 +45,6 @@ class User(db.Model):
         def __repr__(self):
                 return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-
-class Post(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        title = db.Column(db.String(80), nullable=False)
-        date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-        content = db.Column(db.Text, nullable=False)
-        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-        def __repr__(self):
-                return f"User('{self.title}', '{self.date_posted}', '{self.content}')"
 
 
 
