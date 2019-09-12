f51ce6f9dbfb83388e56d114cc2b3ee767b5571a
mwarszczynski <m.warszczynski@gmail.com>
2019-09-12 10:11:11 -0700

    some changes

--- Python/7. Portfolio Projects/Django/1. Django CMS/car/models.py
+++ Python/7. Portfolio Projects/Django/1. Django CMS/car/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 
-class Producer(models.Model):
-    model = models.Charfield(max_length=250)
-    model_enginee = models.Charfield(max_length=500)
-    color = models.Charfield(max_length=100)
-    model_logo = models.Charfield(max_length=1000)
+class Producer(models.Model):                           # Toyota
+    model = models.CharField(max_length=250)            # Corolla
+    model_enginee = models.CharField(max_length=500)    # fuel/oil/diesel
+    color = models.CharField(max_length=100)            # color
+    model_logo = models.CharField(max_length=1000)      # model logo/string
+
+    def __repr__(self):
+        return self.model + ' - ' + self.model_enginee
 
 
-class Brand(models.Model):
-    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
-    file_type = models.Charfield(max_length=10)
+class Brand(models.Model):                                                  # Marka
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)        # relacja - producent
+    file_type = models.CharField(max_length=10)                             # 
     
--- Python/7. Portfolio Projects/Django/1. Django CMS/website/settings.py
+++ Python/7. Portfolio Projects/Django/1. Django CMS/website/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    ''
+    'car.apps.CarConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
